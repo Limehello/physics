8accf04bfb1ed9156e4ff8c6494d760b9d41f94f
Limehello <plxsmazis@gmail.com>
2024-09-03 18:25:29 +0800

    Update PhysicsAngle.java

--- src/main/java/com/advn/physics/PhysicsAngle.java
+++ src/main/java/com/advn/physics/PhysicsAngle.java
@@ -1,33 +1,54 @@
 package com.advn.physics;
 
 public class PhysicsAngle {
-    private double angle;
+    private double angle; // Angle in radians
+
     public PhysicsAngle(double angle) {
         this.angle = angle;
+        normalize(); // Ensure angle is normalized upon creation
     }
+
     public void setAngle(double angle) {
         this.angle = angle;
+        normalize(); // Normalize when setting a new angle
     }
+
     public double getAngle() {
         return this.angle;
     }
+
     public double convertToRadians(double deg) {
-        return deg * (3.141592653589793 / 180)
+        return deg * (Math.PI / 180);
     }
-    public void add(Angle other) {
+
+    public double convertToDegrees(double rad) {
+        return rad * (180 / Math.PI);
+    }
+
+    public void add(PhysicsAngle other) {
         this.angle += other.getAngle();
+        normalize();
     }
-    public void subtract(Angle other) {
+
+    public void subtract(PhysicsAngle other) {
         this.angle -= other.getAngle();
+        normalize();
     }
+
     public void normalize() {
         this.angle = this.angle % (2 * Math.PI);
         if (this.angle < 0) {
             this.angle += 2 * Math.PI;
         }
     }
-    public double angleBetween(Angle other) {
+
+    public double angleBetween(PhysicsAngle other) {
         double difference = Math.abs(this.angle - other.getAngle());
         return Math.min(difference, 2 * Math.PI - difference);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("PhysicsAngle(angle: %.2f radians, %.2f degrees)", angle, convertToDegrees(angle));
     }
 }
