76219dd2843f898d2adebfc358957cbba3235d51
Limehello <plxsmazis@gmail.com>
2024-09-03 16:26:53 +0800

    Update PhysicsAngle.java

--- src/main/java/com/advn/physics/PhysicsAngle.java
+++ src/main/java/com/advn/physics/PhysicsAngle.java
@@ -1,17 +1,33 @@
 package com.advn.physics;
 
 public class PhysicsAngle {
-    private double angleDeg
+    private double angle;
     public PhysicsAngle(double angle) {
-        this.angleDeg = angle;
+        this.angle = angle;
     }
     public void setAngle(double angle) {
-        this.angleDeg = angle;
+        this.angle = angle;
     }
     public double getAngle() {
-        return this.angleDeg;
+        return this.angle;
     }
     public double convertToRadians(double deg) {
-        return deg * (3.1415926535 / 180)
+        return deg * (3.141592653589793 / 180)
+    }
+    public void addAngle(Angle other) {
+        this.angle += other.getAngle();
+    }
+    public void subtractAngle(Angle other) {
+        this.angle -= other.getAngle();
+    }
+    public void normalizeAngle() {
+        this.angle = this.angle % (2 * Math.PI);
+        if (this.angle < 0) {
+            this.angle += 2 * Math.PI;
+        }
+    }
+    public double angleBetween(Angle other) {
+        double difference = Math.abs(this.angle - other.getAngle());
+        return Math.min(difference, 2 * Math.PI - difference);
     }
 }
